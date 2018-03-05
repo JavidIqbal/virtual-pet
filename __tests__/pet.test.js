84a0700508d032c41c52742e1ef13e43da89bf0f
JavidIqbal <jav99@hotmail.com>
2018-03-05 11:35:04 +0000

    Step 6 completed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -6,20 +6,36 @@ describe('constructor', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
 
-    it('sets the name property', () => {
-        expect(pet.name).toEqual('Fido');
+    describe('name', () => {
+        it('sets the name property', () => {
+            expect(pet.name).toEqual('Fido');
+        });
+
+        describe('age', () => {
+            it('sets the age property', () => {
+                expect(pet.age).toEqual(0);
+            });
+
+            describe('hunger', () => {
+                it('sets the hunger property', () => {
+                    expect(pet.hunger).toEqual(0);
+                });
+
+                describe('fitness', () => {
+                    it('sets the fitness property', () => {
+                        expect(pet.fitness).toEqual(10);
+                    });
+
+                    describe('walk', () => {
+                        it('increases fitness to a maximum of 10', () => {
+                            const pet = new Pet('fido');
+                            pet.fitness = 8;
+                            pet.walk();
+                            expect(pet.fitness).toEqual(10);
+                        });
+                    });
+                });
+            });
+        });
     });
-
-    it('sets the age property', () => {
-        expect(pet.age).toEqual(0);
-    });
-
-    it('sets the hunger property', () => {
-        expect(pet.hunger).toEqual(0);
-    });
-
-    it('sets the fitness property', () => {
-        expect(pet.fitness).toEqual(10);
-    })
-
 });
--- src/pet.js
+++ src/pet.js
@@ -14,4 +14,13 @@ function Pet() {
     }
 };
 
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= 10) {
+        this.fitness += 4;
+    } else {
+        this.fitness = 10;
+    }
+}
+
+
 module.exports = Pet;
